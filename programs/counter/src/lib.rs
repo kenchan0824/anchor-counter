9f37b707ec1bb79cf605a1579b2a8cf7e4cc37d2
Kendrick Chan <kenchan0824@gmail.com>
2024-11-07 17:36:58 +0800

    init counter

--- programs/counter/src/lib.rs
+++ programs/counter/src/lib.rs
@@ -6,10 +6,37 @@ declare_id!("Cf9qZuP2WabAWALeqXTKDTnfu8HMqSczFK8AFBDmnzVS");
 pub mod counter {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn init_counter(ctx: Context<InitCounter>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        counter.owner = ctx.accounts.owner.key();
+        counter.count = 0;
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct InitCounter<'info> {
+    
+    #[account(
+        init,
+        seeds = ["counter".as_bytes(), owner.key().as_ref()],
+        bump,
+        space = 8 + Counter::INIT_SPACE,
+        payer = owner
+    )]
+    pub counter: Account<'info, Counter>,
+
+    #[account(mut)]
+    pub owner: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Counter {
+    pub owner: Pubkey,
+    pub count: u64,
+}
--- tests/counter.ts
+++ tests/counter.ts
@@ -1,16 +1,31 @@
 import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
+import { Program, web3, BN} from "@coral-xyz/anchor";
 import { Counter } from "../target/types/counter";
+const assert = require("assert"); 
 
-describe("counter", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+describe("Anchor Counter Program", () => {
+
+  const provider = anchor.AnchorProvider.env()
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.Counter as Program<Counter>;
+  const owner = provider.wallet.publicKey;
+  const [counterPK, bump] = web3.PublicKey.findProgramAddressSync(
+    [Buffer.from("counter"), owner.toBuffer()],
+    program.programId
+  )
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("counter account is initialized properly", async () => {
+    await program.methods.initCounter()
+      .accounts({
+        owner,
+        counter: counterPK
+      })
+      .rpc();
+
+    const counter = await program.account.counter.fetch(counterPK);
+    assert.ok(counter.owner.toBase58() === owner.toBase58());
+    assert.ok(counter.count.toNumber() === 0);
   });
+
 });
